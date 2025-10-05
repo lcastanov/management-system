7d159b4f5e29a316d9409e5d942a7c16df15b632
Laura <lauracastanovilla@usf.edu>
2025-10-05 00:09:10 -0400

    initial class set up

--- cc_6.js
+++ cc_6.js
@@ -1,1 +1,11 @@
 // Coding Challenge 6a
+
+class Employee {
+    constructor(name, department) {
+        this.name = name;
+        this.department = department;
+    }
+    describe() {
+        return `The employee ${this.name} works for the ${this.department} department`;
+    }
+}
