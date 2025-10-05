f261973b1ff02ffb13f9b6a15163f3e7972444af
Laura <lauracastanovilla@usf.edu>
2025-10-05 00:17:38 -0400

    step 2 override

--- cc_6.js
+++ cc_6.js
@@ -1,5 +1,6 @@
 // Coding Challenge 6a
 
+// Step 1
 class Employee {
     constructor(name, department) {
         this.name = name;
@@ -7,5 +8,16 @@ class Employee {
     }
     describe() {
         return `The employee ${this.name} works for the ${this.department} department`;
+    }
+}
+
+// Step 2
+class Manager extends Employee {
+    constructor(name, department, teamSize) {
+        super(name, department);
+        this.teamSize = teamSize
+    }
+    describe(){
+        return `${this.name} manages in the department of ${this.department} a team of ${this.teamSize}`;
     }
 }
